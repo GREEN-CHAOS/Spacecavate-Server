d1d33d39cbe5fd572dbde813e8de1b5a35640aab
haennes <hannes.hofmuth@gmail.com>
2020-11-29 16:37:52 +0100

    added the OrbitCalculator Node
    It now works right

--- Spacecavate-Server/OrbitCalculator.gd
+++ Spacecavate-Server/OrbitCalculator.gd
@@ -6,13 +6,16 @@ extends OrbitCalculator
 # var b = "text"
 var rand = RandomNumberGenerator.new()
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rand.randomize()
-	for i in range(50):
-		var randomVector = Vector3(rand.randf_range(-10000,10000),rand.randf_range(-10000,10000),rand.randf_range(-10000,10000))
-		var randomVector2 = Vector3(rand.randf_range(-10000,10000),rand.randf_range(-10000,10000),rand.randf_range(-10000,10000))
-		self.addSattelite(randomVector,randomVector2,rand.randf_range(1,100),false,Vector3(0,0,0))
+#	rand.randomize()
+#	for i in range(50):
+		var randomVector = Vector3(5,0,0)
+		var randomVector2 = Vector3(0,0,0)
+		self.addSattelite(randomVector2,Vector3(0,0,0),1000,true,Vector3(0,0,0))
+		self.addSattelite(randomVector,Vector3(0,0,3),1000,false,Vector3(0,0,0))
 		
 
 
